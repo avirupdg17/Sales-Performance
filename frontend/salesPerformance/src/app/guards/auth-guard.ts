a990a5d57ef36a286c101575187103f991791f0c
avirupdg17 <avirupduttagupta@gmail.com>
2025-06-03 08:55:44 +0530

    fe changes

--- frontend/salesPerformance/src/app/guards/auth-guard.ts
+++ frontend/salesPerformance/src/app/guards/auth-guard.ts
@@ -1,11 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { UserAuthentication } from '../services/user-authentication';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router=inject(Router);
+  const userAuthenticationService = inject(UserAuthentication);
   if (typeof sessionStorage !== 'undefined') {
     const accessToken = sessionStorage.getItem('access_token');
+    const userRole = sessionStorage.getItem('sp_role') || '';
     if (accessToken) {
+      userAuthenticationService.setAccessDetails(accessToken, userRole);
       return true; // Allow access to the route
     }
   }
